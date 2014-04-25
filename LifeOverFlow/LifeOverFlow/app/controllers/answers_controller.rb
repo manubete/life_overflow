e87ab30964851a6295c8888c169071800602adbc
Erin Joan Snyder & Ashwin Kumar <pair+ErinJoan.ashnkumar@devbootcamp.com>
2014-04-25 12:32:08 -0700

    Removed extra puts from answers controller test

--- LifeOverFlow/LifeOverFlow/app/controllers/answers_controller.rb
+++ LifeOverFlow/LifeOverFlow/app/controllers/answers_controller.rb
@@ -8,8 +8,6 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find params[:question_id]
     @answer = @question.answers.build params[:answer]
-    puts "---HERE IS ANSWER______"
-    puts @answer.inspect
     if @answer.save
       redirect_to question_path(@question)
     else
