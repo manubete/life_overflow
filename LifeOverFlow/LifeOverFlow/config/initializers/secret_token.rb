66fe0cf2f801177fe8a6eb0c1525d1e1c3f0f440
Apprentice <apprentice@dbc05.local>
2014-04-24 18:04:17 -0700

    Added Rails Skeleton   *updated database file   *updated gemfile   *tests set up

---
+++ LifeOverFlow/LifeOverFlow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeOverFlow::Application.config.secret_token = '7504783f76bd37a5f38046a085f8b2812c87857ba852c9e01d80add7694634c9d09661c07957d212f112005f5477bb8f2854cb304e8a5117bfc04fda3fbd0872'
