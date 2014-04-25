4a1c28264eea663eddf47bae25b60f1fe6f19843
Erin Joan Snyder & Ashwin Kumar <pair+ErinJoan.ashnkumar@devbootcamp.com>
2014-04-25 10:24:48 -0700

    Fixed user login test

--- LifeOverFlow/LifeOverFlow/spec/features/user_features_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/features/user_features_spec.rb
@@ -29,7 +29,7 @@ feature 'Guest users' do
         fill_in "Email", :with => user.email
         fill_in 'Password', :with => user.password
         click_on "Log in"
-        expect(page).to have_content "Logged in as #{user.email}."
+        expect(page).to have_content "#{user.username}"
       end
     end
   end
