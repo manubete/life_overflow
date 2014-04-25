025d2ff0332d6717698a4999ddf7b64c0c39e4aa
Apprentice <apprentice@dbc05.local>
2014-04-24 22:40:39 -0700

    Added a test to Questions model, to test that it has many answers

--- LifeOverFlow/LifeOverFlow/spec/models/question_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/models/question_spec.rb
@@ -4,5 +4,6 @@ describe Question do
   context "Guest User" do
     it { should allow_mass_assignment_of :question_content}
     it { should validate_presence_of :question_content }
+    it { should have_many(:answers)}
   end
 end
