bed9d64571caa138c2065992ab3c6414a2449ac3
Apprentice <apprentice@dbc05.local>
2014-04-24 20:08:46 -0700

    Created Answers Route nested under Questions and AnswersController   * Added temporary link_to button for Answers from Question view   * (took out nested route)
    TODO: add controller tests for new routes

--- LifeOverFlow/LifeOverFlow/config/routes.rb
+++ LifeOverFlow/LifeOverFlow/config/routes.rb
@@ -1,4 +1,7 @@
 LifeOverFlow::Application.routes.draw do
   root :to => "questions#index"
   resources :questions
+  # do
+  #   resources :answers
+  # end
 end
--- LifeOverFlow/LifeOverFlow/spec/features/post_answer_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/features/post_answer_spec.rb
@@ -4,7 +4,7 @@ describe "User can post a answer" do
   context "User logged into a question page" do
   let(:answer) {FactoryGirl.create :answer}
     it "can submit answer for question and see answer on page" do
-      visit question_path # Check this out
+      visit question_path(:id => 1) # Check this out
       click_on 'Answer'
       fill_in 'answer[content]', with: answer.body
       click_on 'Submit'
