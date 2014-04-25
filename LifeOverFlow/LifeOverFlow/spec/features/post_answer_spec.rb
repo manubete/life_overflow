1563005f6ff41cbd7b7357396fd383d178f1dd51
Apprentice <apprentice@dbc05.local>
2014-04-24 22:38:49 -0700

    Fixed errors in Feature Test   *used corrected column and attributes names   *fixed submit button reference

--- LifeOverFlow/LifeOverFlow/spec/features/post_answer_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/features/post_answer_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe "User can post a answer" do
   context "User logged into a question page" do
-  let(:answer) {FactoryGirl.create :answer}
+  let(:question) {FactoryGirl.create :question}
+  let(:answer) {FactoryGirl.create :answer }
     it "can submit answer for question and see answer on page" do
-      visit question_path(:id => 1) # Check this out
+      visit question_path(:id => question.id) # Check this out
       click_on 'Answer'
-      fill_in 'answer[content]', with: answer.body
+      fill_in 'answer[answer_content]', with: answer.answer_content
       click_on 'Submit'
-      expect(page).to have_content answer.body
+      expect(page).to have_content answer.answer_content
     end
   end
 end
