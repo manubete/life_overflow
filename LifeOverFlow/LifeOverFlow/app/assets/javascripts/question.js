3942be1252148cac8f6aa0e5a8200596000eb573
Apprentice <apprentice@dbc30.local>
2014-04-26 15:16:10 -0700

    Add a listener for the new question button   *add listender for ajax success   *add ability to toggle form from hidden to not hidden by clicking on create question button

--- LifeOverFlow/LifeOverFlow/app/assets/javascripts/question.js
+++ LifeOverFlow/LifeOverFlow/app/assets/javascripts/question.js
@@ -1,13 +1,19 @@
 var Question = {
   init: function() {
-    $('a.new-question').on('click',this.toggleQuestionForm)
+    $('a.new_question').on('click',this.toggleQuestionForm);
+    $('form#new_question').on('ajax:success', this.appendQuestion)
   },
+
   toggleQuestionForm: function(e) {
     e.preventDefault();
-    $('form#new_question').toggleClass'hidden');
+    $('form#new_question').toggleClass('hidden');
+  },
+
+  appendQuestion: function(event, data) {
+
   }
 }
 
 $(document).ready(function() {
   Question.init();
-}
+})
