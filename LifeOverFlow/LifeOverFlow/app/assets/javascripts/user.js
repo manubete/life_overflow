f61567e320ccbdb876ffd9fdb6217b76e67fa17e
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-26 17:22:09 -0700

    basic css styling on every view page

--- LifeOverFlow/LifeOverFlow/app/assets/javascripts/user.js
+++ LifeOverFlow/LifeOverFlow/app/assets/javascripts/user.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-  $('a#sign_up_btn').on('click', this.toggleUserSignUp);
-})
+// $(document).ready(function(){
+//   $('a#sign_up_btn').on('click', this.toggleUserSignUp);
+// })
 
-toggleUserSignUp: function(e){
-  e.preventDefault();
-  $('form').toggleClass('hidden');
-},
+// toggleUserSignUp: function(e){
+//   e.preventDefault();
+//   $('form').toggleClass('hidden');
+// },
 
-appendSignUp: function(event, data, status, xhr){
-  $('div#main_container').append(data);
-},
-appendErrors: function(event, xhr, status, error) {
-    $('div#main_container').before(xhr.responseText);
-  }
-}
+// appendSignUp: function(event, data, status, xhr){
+//   $('div#main_container').append(data);
+// },
+// appendErrors: function(event, xhr, status, error) {
+//     $('div#main_container').before(xhr.responseText);
+//   }
+// }
