66b3874af2930d34927c21b6531cfde23dd2ca54
Apprentice <apprentice@dbc30.local>
2014-04-27 14:31:14 -0700

    comments are now ajaxed

--- LifeOverFlow/LifeOverFlow/app/assets/javascripts/comment.js
+++ LifeOverFlow/LifeOverFlow/app/assets/javascripts/comment.js
@@ -1,26 +1,51 @@
 var Comment = {
   init: function() {
-    $('a.new_comment_on_question').on('click', this.toggleCommentForm);
-    $('form#new_comment').on('ajax:success', this.appendComment);
-    $('form#new_comment').on('ajax:error', this.appendErrors);
+    $('a.new_comment_on_question').on('click', this.toggleQuestionCommentForm);
+    $('a.new_comment_on_answer').on('click', this.toggleAnswerCommentForm);
+    $('.questionCommentForm').on('ajax:success', this.appendQuestionComment);
+    $('.questionCommentForm').on('ajax:error', this.appendErrors);
+    $('.answerCommentForm').on('ajax:success', this.appendAnswerComment);
+    $('.answerCommentForm').on('ajax:error', this.appendErrors);
   },
 
-  toggleCommentForm: function(e) {
+  toggleQuestionCommentForm: function(e) {
     e.preventDefault();
-    $('form#new_comment').toggleClass('hidden');
+    if ($('.questionCommentForm').is(':hidden')) {
+      $('.questionCommentForm').slideDown()
+    } else {
+      $('.questionCommentForm').slideUp()
+    }
   },
 
-  appendComment: function(event, data) {
-    console.log("BBBBBAHHHH");
-    $('#show_comments').append(data);
-    $('form#new_comment').toggleClass('hidden');
+  toggleAnswerCommentForm: function(e) {
+
+    e.preventDefault();
+    if ($('.answerCommentForm').is(':hidden')) {
+      $('.answerCommentForm').slideDown()
+    } else {
+      $('.answerCommentForm').slideUp()
+    }
+  },
+
+  appendQuestionComment: function(event, data) {
+    $('.questionCommentForm').hide()
+    $('.questionCommentForm')[0].reset();
+    $('#show_question_comments').append(data);
+  },
+
+  appendAnswerComment: function(event, data) {
+    $('.answerCommentForm').hide()
+    $('.answerCommentForm')[0].reset();
+    $('#show_answer_comments').append(data);
   },
 
   appendErrors: function(event, xhr, status, error) {
-    console.log("YEEEHAW");
+    $('#comment_question_errors').append(xhr.responseText);
   }
 }
 
 $(document).ready(function() {
-  Comment.init();
-})
+  Comment.init()
+  $('.questionCommentForm').hide()
+  $('.answerCommentForm').hide()
+});
