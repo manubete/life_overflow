4584a367467ed73458a80c9c8537951e0e94dc61
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-27 14:33:10 -0700

    Don't like the way the toggled form looks, so I just rendered a partial on the signup page instead. Much cleaner, even if it's less dynamic.

--- LifeOverFlow/LifeOverFlow/app/assets/javascripts/user.js
+++ LifeOverFlow/LifeOverFlow/app/assets/javascripts/user.js
@@ -1,26 +1,26 @@
-var SignUp = {
+// var SignUp = {
 
-  bindEvents: function(){
-    $('a#sign_up_btn').on('click', this.toggleSignUp);
-  },
+//   bindEvents: function(){
+//     $('a#sign_up_btn').on('click', this.toggleSignUp);
+//   },
 
-  toggleSignUp: function(e){
-  $('div#signup_form').toggleClass('hidden');
-  },
+//   toggleSignUp: function(e){
+//   $('div#signup_form').toggleClass('hidden');
+//   },
 
-  appendSignUp: function(event, data) {
-    console.log("appending the signup form")
-      $('.error_messages').html('')
-      $('form#user')[0].reset()
-      $('#signup_form').append(data);
-      $('form#new_user').toggleClass('hidden');
-    },
+//   appendSignUp: function(event, data) {
+//     console.log("appending the signup form")
+//       $('.error_messages').html('')
+//       $('form#user')[0].reset()
+//       $('#signup_form').append(data);
+//       $('form#new_user').toggleClass('hidden');
+//     },
 
-    appendErrors: function(event, xhr, status, error) {
-      $('.error_messages').html('')
-      $('.error_messages').append(xhr.responseText);
-    }
-}
-$(document).ready(function(){
-  SignUp.bindEvents();
-})
+//     appendErrors: function(event, xhr, status, error) {
+//       $('.error_messages').html('')
+//       $('.error_messages').append(xhr.responseText);
+//     }
+// }
+// $(document).ready(function(){
+//   SignUp.bindEvents();
+// })
