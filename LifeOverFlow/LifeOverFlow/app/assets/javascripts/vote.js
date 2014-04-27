334a2d20e7217d70324df5744d1e3d6915655cd2
Apprentice <apprentice@dbc31.local>
2014-04-27 15:33:00 -0700

    Removed the Vanilla javascript for votes to replace with rails ajax

--- LifeOverFlow/LifeOverFlow/app/assets/javascripts/vote.js
+++ LifeOverFlow/LifeOverFlow/app/assets/javascripts/vote.js
@@ -1,31 +1,14 @@
 var Vote = {
   init: function() {
-    $('a.vote').on('click', this.upVote);
-  },
-
- upVote: function(e) {
-  e.preventDefault();
-  vote = $(this)
-  classes = vote.attr('class').split(' ')
-  voteable_type = classes[1]
-  type = classes[2]
-  voteable_id = vote.attr('data-id')
-
-  vote_data = {
-    voteable_type: voteable_type,
-    type: type,
-    voteable_id: voteable_id
-  }
-  debugger
-  $.ajax({
-    type: 'post',
-    url: '/votes',
-    data: vote_data
-  }).done(VoteView.renderVote);
+    // $('a.vote').on('click', this.upVote);
+    $('a.vote').on('ajax:success', VoteView.renderVote);
+     $('a.vote').on('ajax:error', console.log("Errors"));
   }
 }
 
+
 var VoteView = {
+
   renderVote: function(message) {
     console.log("VoteLogged");
   }
