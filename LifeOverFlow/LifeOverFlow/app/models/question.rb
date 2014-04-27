9938330440168364eb09750ae1c618e01cd872ae
Apprentice <apprentice@dbc30.local>
2014-04-26 23:08:13 -0700

    Created empty Question method to total votes Updated routes to have votes as polymorphic under questions, comments, answers

--- LifeOverFlow/LifeOverFlow/app/models/question.rb
+++ LifeOverFlow/LifeOverFlow/app/models/question.rb
@@ -6,6 +6,11 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable
   has_many :answers
+  has_many :votes, :as => :voteable
+
+  def votes_total
+
+  end
 
 end
 
--- LifeOverFlow/LifeOverFlow/spec/models/question_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/models/question_spec.rb
@@ -7,4 +7,20 @@ describe Question do
     it { should have_many(:answers)}
     it { should have_many (:comments)}
   end
+
+  context "Should have many votes" do
+    it { should have_many (:votes)}
+  end
 end
+
+
+describe '#votes_total' do
+
+  context "one vote for a question" do
+    # let(:vote) {FactoryGirl.create :vote}
+    # let(:question) {FactoryGirl.create :question}
+    # # expect { vote.question.votes_total }.to eq(1)
+    # question.votes << vote
+  end
+end
+
