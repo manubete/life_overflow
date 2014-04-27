67cdb5da4a0e16c543322f97f496737a08d3f981
Apprentice <apprentice@dbc31.local>
2014-04-27 15:39:31 -0700

    Added attempt for controller test

---
+++ LifeOverFlow/LifeOverFlow/spec/controllers/votes_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+  describe VotesController do
+    context "#create" do
+      let(:vote_attributes) { FactoryGirl.attributes_for :vote }
+
+      it "creates an up vote for a question with valid attributes" do
+
+        expect {
+          post :create, :vote => vote_attributes
+        }.to change { Vote.count }.by(0)
+      end
+
+      # it "does not create an account with invalid attributes" do
+      #   expect {
+      #     post :create, :user => invalid_user
+      #   }.not_to change { User.count }
+      # end
+    end
+  end
