1b3258f722fc7eb5b4f70fb6aa789478e3bbac12
Apprentice <apprentice@dbc31.local>
2014-04-27 15:49:17 -0700

    Removed Controller test and updated feature test for votes

--- LifeOverFlow/LifeOverFlow/spec/features/vote_on_question_spec.rb
+++ LifeOverFlow/LifeOverFlow/spec/features/vote_on_question_spec.rb
@@ -6,8 +6,9 @@ describe "A User can vote" do
     let!(:user) {FactoryGirl.create :user}
       it 'can vote up on a question' do
         ApplicationController.any_instance.stub(:current_user).and_return(user)
-        visit root_path
-        expect { click_on ".question#vote up" }.to change(question.votes_total).by(1)
+          visit root_path
+          click_on "UP"
+          expect(page).to have_content Question.first.vote_total
       end
   end
 end
