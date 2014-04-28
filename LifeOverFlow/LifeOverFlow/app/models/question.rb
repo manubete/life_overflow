0aec95a69d00aef53b0bb666b8c34d93854f2f8f
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-27 18:28:08 -0700

    working on converting epoch datetime to a more readable formate

--- LifeOverFlow/LifeOverFlow/app/models/question.rb
+++ LifeOverFlow/LifeOverFlow/app/models/question.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Question < ActiveRecord::Base
   attr_accessible :question_content, :question_title, :user_id
 
@@ -6,6 +8,15 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable
   has_many :answers
+
+  def timestamp_to_time(timestamp)
+    now = Time.now
+    p "$"*50
+    puts now
+    p "$"*50
+    Time.at(1234567890).strftime("%m/%d/%Y")
+    DateTime.strptime(Time.now.utc.to_i.to_s,'%s').strftime("%d/%m/%y ")
+  end
 
 end
 
